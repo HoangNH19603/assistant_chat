d7334648e62711b1f149fbcb40d2a3d2e409dda1
Nguyen Huy Hoang <19t6n2003@gmail.com>
2024-03-10 01:30:29 +0700

    update

--- lib/services/bard_services.dart
+++ lib/services/bard_services.dart
@@ -3,12 +3,21 @@ import 'package:chat_with_ai/const/api_key.dart';
 
 final class BardService {
   final GenerativeModel model;
+  late ChatSession chat;
 
-  BardService() : model = GenerativeModel(model: "gemini-pro", apiKey: KEY.GEMINI.key);
+  BardService() : model = GenerativeModel(model: "gemini-pro", apiKey: KEY.GEMINI.key) {
+    chat = model.startChat();
+  }
 
   Future<String> ask(String prompt) async {
-    final List<Content> content = [Content.text(prompt)];
-    final GenerateContentResponse response = await model.generateContent(content);
+    // final List<Content> content = [Content.text(prompt)];
+    final Content content = Content.text(prompt);
+    final GenerateContentResponse response = await chat.sendMessage(content);
+    // final GenerateContentResponse response = await model.generateContent(content);
     return response.text ?? "Sorry! I can't find the answer for your question right now!";
   }
+
+  // void newSession() async {
+  //   model.startChat();
+  // }
 }
